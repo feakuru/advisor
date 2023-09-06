01907daaa2bb62a9404909844f3f338fb4d2fb12
Dmitry Orlov <feanarokurufinve@gmail.com>
2023-09-07 03:59:40 +0400

    Changed RegressionAdvisor into DNNAdvisor

--- advisors/dnn.py
+++ advisors/dnn.py
@@ -8,18 +8,19 @@ from common.utils import get_logger
 logger = get_logger('advisor-regr')
 
 
-class RegressionAdvisor(BaseAdvisor):
+class DNNAdvisor(BaseAdvisor):
     def get_model(self) -> tf.keras.Model:
         normalizer = tf.keras.layers.Normalization()
         normalizer.adapt(np.array(self.train_features))
 
-        model = tf.keras.Sequential([
-            normalizer,
-            tf.keras.layers.Dense(units=1)
-        ])
+        layers = [normalizer]
+        for _ in range(2):
+            layers.append(tf.keras.layers.Dense(24, activation='relu'))
+        layers.append(tf.keras.layers.Dense(1))
+        model = tf.keras.Sequential(layers)
 
         model.compile(
-            optimizer=tf.keras.optimizers.Adam(learning_rate=0.1),
+            optimizer=tf.keras.optimizers.Adamax(learning_rate=0.001),
             loss='mean_absolute_error',
         )
 
